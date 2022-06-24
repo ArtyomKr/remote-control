91940baf90ad572579db5cb38c9f43983564880b
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-06-24 12:29:49 +0300

    feat: add exit handler for windows

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { httpServer } from './src/http_server/index.js';
+import readline from 'readline';
 import 'dotenv/config';
 import wsServer from './src/webSocket/index.js';
 
 const HTTP_PORT = process.env.PORT;
 
 httpServer.listen(HTTP_PORT, () => {
-  console.log(`Server running at http://localhost:${HTTP_PORT}/`);
+  console.log(`Front-end running at http://localhost:${HTTP_PORT}/`);
 });
 
 wsServer;
@@ -14,5 +15,17 @@ process.on('SIGINT', () => {
   wsServer.clients.forEach((socket) => {
     socket.close();
   });
-  process.exit(0);
+
+  setTimeout(() => process.exit(0), 500);
 })
+
+if (process.platform === 'win32') {
+  const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+  });
+  
+  rl.on("SIGINT", function () {
+    process.emit('SIGINT', 'SIGINT');
+  });
+}
