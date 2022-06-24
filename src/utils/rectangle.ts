34393c7a6e5f8df01aed212f1de59e0b942164d8
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-06-24 12:32:35 +0300

    refactor: run eslint --fix

--- src/utils/rectangle.ts
+++ src/utils/rectangle.ts
@@ -1,15 +1,19 @@
 import robot from 'robotjs';
 
-export default function drawRectangle(a: number, b: number, pos: { x: number, y: number }) {
+export default function drawRectangle(
+  a: number,
+  b: number,
+  pos: { x: number; y: number }
+) {
   let { x, y } = pos;
   robot.setMouseDelay(100);
-  robot.mouseToggle("down");
+  robot.mouseToggle('down');
 
-  robot.dragMouse(x += a, y);
-  robot.dragMouse(x, y += b);
-  robot.dragMouse(x -= a, y);
-  robot.dragMouse(x, y -=b);
-  
-  robot.mouseToggle("up");
+  robot.dragMouse((x += a), y);
+  robot.dragMouse(x, (y += b));
+  robot.dragMouse((x -= a), y);
+  robot.dragMouse(x, (y -= b));
+
+  robot.mouseToggle('up');
   robot.setMouseDelay(10);
-};
+}
--- src/webSocket/index.ts
+++ src/webSocket/index.ts
@@ -1,23 +1,29 @@
-import fs from 'fs';
 import robot from 'robotjs';
-import Jimp from 'jimp';
 import { WebSocketServer, createWebSocketStream } from 'ws';
 import 'dotenv/config';
 import drawCircle from '../utils/circle.js';
 import drawRectangle from '../utils/rectangle.js';
+import takeScreen from '../utils/screen.js';
 
 const WS_PORT = process.env.WS_PORT;
 
 const wsServer = new WebSocketServer({ port: Number(WS_PORT) });
 
-wsServer.on("connection", (socket, req) => {
-  const duplex = createWebSocketStream(socket, { encoding: 'utf8', decodeStrings: false });
-  duplex.on('data', (data) => console.log(data));
+wsServer.on('connection', (socket, req) => {
+  const duplex = createWebSocketStream(socket, {
+    encoding: 'utf8',
+    decodeStrings: false
+  });
 
-  console.log(`Established connection with ${req.socket.remoteAddress}`);
-  socket.send('Connection_established\0');
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    `Established WebSocket connection with ${req.headers.origin}\0`
+  );
+  console.log('\x1b[34m%s\x1b[0m', `Params: ${req.rawHeaders}`);
+  duplex.write(`Connected_to_${req.headers.host}\0`);
 
-  socket.on("message", (data) => {
+  duplex.on('data', (data) => {
+    console.log(`\x1b[35mreceived\x1b[0m: ${data}`);
     const packet = data.toString().split(' ');
     const command = packet[0];
     const mousePos = robot.getMousePos();
@@ -34,15 +40,15 @@ wsServer.on("connection", (socket, req) => {
         break;
       case 'mouse_right':
         robot.moveMouse(mousePos.x + Number(packet[1]), mousePos.y);
-        break;  
+        break;
       case 'mouse_position':
-        const res = `mouse_position ${mousePos.x},${mousePos.y}`;
+        const res = `mouse_position ${mousePos.x},${mousePos.y}\0`;
         duplex.write(res);
-        duplex.push(res);
+        console.log(`\x1b[36msend\x1b[0m: ${res}`);
         break;
       case 'draw_circle':
         drawCircle(Number(packet[1]), mousePos);
-        break;  
+        break;
       case 'draw_rectangle':
         drawRectangle(Number(packet[2]), Number(packet[1]), mousePos);
         break;
@@ -50,12 +56,17 @@ wsServer.on("connection", (socket, req) => {
         drawRectangle(Number(packet[1]), Number(packet[1]), mousePos);
         break;
       case 'prnt_scrn':
-        break;  
+        takeScreen(mousePos, 200).then((screen) => {
+          const res = `prnt_scrn ${screen}\0`;
+          duplex.write(res);
+          console.log(`\x1b[36msend\x1b[0m: ${res.slice(0, 100)}...`);
+        });
+        break;
     }
   });
 
   socket.on('close', () => {
-    console.log(`${req.socket.remoteAddress} disconnected`);
+    console.log(`${req.headers.origin} disconnected\0`);
   });
 });
 
