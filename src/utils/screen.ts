7055cd172f651257e3b4b359f42ef0443bb2a6b4
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-06-24 12:31:43 +0300

    feat: add working screen function

--- src/utils/screen.ts
+++ src/utils/screen.ts
@@ -1,5 +1,29 @@
+import Jimp from 'jimp';
 import robot from 'robotjs';
 
-export default function takeScreen(pos: { x: number, y: number }) {
-  const screeen = robot.screen.capture(pos.x, pos.y, 200, 200);
+export default async function takeScreen(
+  pos: { x: number; y: number },
+  size: number
+) {
+  const rawScreen = robot.screen.capture(
+    pos.x - size / 2,
+    pos.y - size / 2,
+    size,
+    size
+  );
+
+  for (let i = 0; i < rawScreen.image.length; i += 4) {
+    if (i % 4 === 0) {
+      [rawScreen.image[i], rawScreen.image[i + 2]] = [
+        rawScreen.image[i + 2],
+        rawScreen.image[i]
+      ];
+    }
+  }
+
+  const jimp = new Jimp(rawScreen.width, rawScreen.height);
+  jimp.bitmap.data = rawScreen.image;
+
+  const base64 = await jimp.getBase64Async(Jimp.MIME_PNG);
+  return base64.split(',')[1];
 }
