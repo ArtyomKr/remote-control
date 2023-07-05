7f29752bf0577c91aa19ec3abc48ff36ef2706f8
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-05 22:39:47 +0300

    refactor: optimize roomHandler code

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,27 +1,28 @@
 import { IAddPlayerReq, ICreateGameRes, ICreateRoomReq, IUpdateRoomRes } from '../models/index.js';
-import { addPlayerToRoom, createRoom } from '../dbHelpers/index.js';
+import { addPlayerToRoom, createRoom, createGame, deleteRoom } from '../dbHelpers/index.js';
+import updateRoomsRes from './updateRooms.js';
 
-function roomHandler(req: ICreateRoomReq | IAddPlayerReq): IUpdateRoomRes | ICreateGameRes | undefined {
+function roomHandler(req: ICreateRoomReq | IAddPlayerReq): IUpdateRoomRes | ICreateGameRes {
   if (req.type === 'create_room') {
-    const { roomId, roomUsers } = createRoom(req.id);
-    return {
-      type: 'create_game',
-      data: {
-        idGame: roomId,
-        idPlayer: roomUsers[0].index,
-      },
-      id: 0,
-    };
+    createRoom();
   }
-  if (req.type === 'add_player_to_room') {
+  if (req.type === 'add_user_to_room') {
     const { indexRoom } = req.data;
-    const rooms = addPlayerToRoom(indexRoom, req.id)
-    return {
-      type: "update_room",
-      data: rooms,
-      id: 0,
+    const room = addPlayerToRoom(indexRoom, req.id);
+    if (room.roomUsers.length >= 2) {
+      deleteRoom(indexRoom);
+      const game = createGame(room.roomUsers);
+      return {
+        type: 'create_game',
+        data: {
+          idGame: game.gameId,
+          idPlayer: req.id === game.players[0].index ? game.players[1].index : game.players[0].index,
+        },
+        id: req.id,
+      };
     }
   }
+  return updateRoomsRes();
 }
 
 export default roomHandler;
