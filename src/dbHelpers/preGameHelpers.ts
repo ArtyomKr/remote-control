f44df69e0cfff1d1216dda0c98a1289843d76a20
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-06 23:19:30 +0300

    feat: run lint, implement ship position parsing to db

--- src/dbHelpers/preGameHelpers.ts
+++ src/dbHelpers/preGameHelpers.ts
@@ -3,7 +3,10 @@ import { IGame, IShip, IUser } from '../models/index.js';
 
 function createGame(players: Omit<IUser, 'password'>[]) {
   const db = getDB();
-  (<IGame['players']>players).forEach((player) => (player.ships = []));
+  (<IGame['players']>players).forEach((player) => {
+    player.ships = [];
+    player.placedShips = [];
+  });
   const game = {
     gameId: db.games.length,
     players: <IGame['players']>players,
@@ -17,13 +20,26 @@ function createGame(players: Omit<IUser, 'password'>[]) {
 function addShips(playerId: number, gameId: number, shipsArr: IShip[]) {
   const db = getDB();
   const game = db.games.find(({ gameId: id }) => id === gameId);
-  game?.players.map((player) => { 
-    if (player.index === playerId) player.ships = shipsArr;
-    return player;
+  game?.players.forEach((player) => {
+    if (player.index === playerId) {
+      player.ships = shipsArr;
+      player.placedShips = placeShips(shipsArr);
+    }
   });
 
   setDB(db);
   return game;
+}
+
+function placeShips(ships: IShip[]) {
+  return ships.map(({ direction, position, length }) => {
+    const empty = new Array(length).fill(null);
+    const pos = direction
+      ? empty.map((el, i) => ({ x: position.x, y: position.y + i }))
+      : empty.map((el, i) => ({ x: position.x + i, y: position.y }));
+
+    return { pos, killed: false };
+  });
 }
 
 export { createGame, addShips };
--- src/handlers/preGameHandler.ts
+++ src/handlers/preGameHandler.ts
@@ -1,24 +1,26 @@
-import { addShips } from "../dbHelpers/index.js";
-import { IAddShipsReq, IStartGameRes } from "../models/index.js";
+import { addShips } from '../dbHelpers/index.js';
+import { IAddShipsReq, IStartGameRes } from '../models/index.js';
 
-function preGameHandler(req: IAddShipsReq) : { type: 'start_game', resArr: IStartGameRes[]} | undefined {
+function preGameHandler(req: IAddShipsReq): { type: 'start_game'; resArr: IStartGameRes[] } | undefined {
   const { indexPlayer, gameId, ships } = req.data;
   const game = addShips(indexPlayer, gameId, ships);
-  const readyPlayers = game?.players.filter(({ ships }) => ships.length > 0)?? []
+  const readyPlayers = game?.players.filter(({ ships }) => ships.length > 0) ?? [];
   if (readyPlayers.length === game?.players.length) {
-    const resArr = readyPlayers.map(({ index, ships }): IStartGameRes => ({
-      type: 'start_game',
-      data: {
-        ships: ships,
-        currentPlayerIndex: index,
-      },
-      id: index
-    }));
+    const resArr = readyPlayers.map(
+      ({ index, ships }): IStartGameRes => ({
+        type: 'start_game',
+        data: {
+          ships: ships,
+          currentPlayerIndex: index,
+        },
+        id: index,
+      }),
+    );
 
     return {
       type: 'start_game',
-      resArr
-    }
+      resArr,
+    };
   }
 }
 
