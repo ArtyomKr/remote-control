611bb6aefb32571484f25738ea2ab0f38e18ff4c
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-06 12:37:00 +0300

    feat: add template for game helpers

--- src/dbHelpers/gameHelpers.ts
+++ src/dbHelpers/gameHelpers.ts
@@ -1,17 +1,8 @@
-import { getDB, setDB } from '../db/index.js';
-import { IGame, IUser } from '../models/index.js';
+import { getDB } from "../db";
 
-function createGame(players: Omit<IUser, 'password'>[]) {
-  const db = getDB();
-  (<IGame['players']>players).forEach((player) => (player.ships = []));
-  const game = {
-    gameId: db.games.length,
-    players: <IGame['players']>players,
-  };
-  db.games.push(game);
-  setDB(db);
-
-  return game;
+function attack(gameId: number, playerId: number, coordinates: { x: number, y: number }) {
+  const bd = getDB();
+  
 }
 
-export { createGame };
+export { attack };
--- src/dbHelpers/index.ts
+++ src/dbHelpers/index.ts
@@ -1,3 +1,4 @@
 export { addUser, findUserByName, findUserById } from './userHelpers.js';
 export { createRoom, addPlayerToRoom, deleteRoom } from './roomHelpers.js';
-export { createGame } from './gameHelpers.js';
+export { createGame, addShips } from './preGameHelpers.js';
+export { attack } from './gameHelpers.js'
