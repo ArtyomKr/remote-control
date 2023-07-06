034d6ff9ed7fab26287ed4c9d811a89f9162540e
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-06 23:20:16 +0300

    feat: run lint

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -2,7 +2,9 @@ import { IAddPlayerReq, ICreateGameRes, ICreateRoomReq, IUpdateRoomRes } from '.
 import { addPlayerToRoom, createRoom, createGame, deleteRoom } from '../dbHelpers/index.js';
 import updateRoomsRes from './updateRooms.js';
 
-function roomHandler(req: ICreateRoomReq | IAddPlayerReq): { type: 'create_game',  resArr: ICreateGameRes[]} | IUpdateRoomRes {
+function roomHandler(
+  req: ICreateRoomReq | IAddPlayerReq,
+): { type: 'create_game'; resArr: ICreateGameRes[] } | IUpdateRoomRes {
   if (req.type === 'create_room') {
     createRoom();
   }
@@ -12,18 +14,20 @@ function roomHandler(req: ICreateRoomReq | IAddPlayerReq): { type: 'create_game'
     if (room.roomUsers.length >= 2) {
       deleteRoom(indexRoom);
       const game = createGame(room.roomUsers);
-      const resArr = game.players.map(({ index }): ICreateGameRes => ({
-        type: 'create_game',
-        data: {
-          idGame: game.gameId,
-          idPlayer: index,
-        },
-        id: index,
-      }));
+      const resArr = game.players.map(
+        ({ index }): ICreateGameRes => ({
+          type: 'create_game',
+          data: {
+            idGame: game.gameId,
+            idPlayer: index,
+          },
+          id: index,
+        }),
+      );
       return {
         type: 'create_game',
         resArr,
-      }
+      };
     }
   }
   return updateRoomsRes();
