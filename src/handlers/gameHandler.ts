49c464c2ccee3fc75c8e9a0d1e54f7ccc47cb9dc
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-07 22:01:53 +0300

    feat: add attack array response to game handler

--- src/handlers/gameHandler.ts
+++ src/handlers/gameHandler.ts
@@ -1,23 +1,46 @@
 import { IAttackReq, IAttackRes } from '../models/index.js';
 import { attack, calcSplashWave } from '../dbHelpers/index.js';
 
-function gameHandler(req: IAttackReq): { type: 'attack_multiple', resArr: IAttackRes[]} | IAttackRes {
-  const { gameId, indexPlayer, x, y } = req.data;
+function gameHandler(req: IAttackReq): { type: 'attack'; resArr: IAttackRes[] } {
+  const attackArr = createAttackArr(req.data);
+
+  const resArr = attackArr.map(
+    ({ tile, status }): IAttackRes => ({
+      type: 'attack',
+      data: {
+        position: {
+          x: tile.x,
+          y: tile.y,
+        },
+        currentPlayer: req.data.indexPlayer,
+        status: status,
+      },
+      id: 0,
+    }),
+  );
+
+  return {
+    type: 'attack',
+    resArr,
+  };
+}
+
+function createAttackArr(
+  data: IAttackReq['data'],
+): { tile: { x: number; y: number }; status: 'miss' | 'killed' | 'shot' }[] {
+  const { gameId, indexPlayer, x, y } = data;
   const { target, status } = attack(gameId, indexPlayer, { x, y });
+  const regularAttack = [{ tile: { x, y }, status }];
+
   if (status === 'killed' && target) {
     const splashCoord = calcSplashWave(target);
-  } else return {
-    type: 'attack',
-    data: {
-      position: {
-        x,
-        y,
-      },
-      currentPlayer: indexPlayer,
-      status: status,
-    },
-    id: req.id,
-  };
+    const splashAttacks = splashCoord.map(({ x, y }) => {
+      const { status } = attack(gameId, indexPlayer, { x, y });
+      return { tile: { x, y }, status };
+    });
+
+    return regularAttack.concat(splashAttacks);
+  } else return regularAttack;
 }
 
 export default gameHandler;
