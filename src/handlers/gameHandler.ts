1e963c11bcebfa71ae54ee438c3d24d92344c24c
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-07 12:46:31 +0300

    fix: remove comment

--- src/handlers/gameHandler.ts
+++ src/handlers/gameHandler.ts
@@ -1,27 +1,23 @@
 import { IAttackReq, IAttackRes } from '../models/index.js';
 import { attack, calcSplashWave } from '../dbHelpers/index.js';
 
-function gameHandler(req: IAttackReq): { type: 'attack_multiple'; resArr: IAttackRes[] } | IAttackRes {
+function gameHandler(req: IAttackReq): { type: 'attack_multiple', resArr: IAttackRes[]} | IAttackRes {
   const { gameId, indexPlayer, x, y } = req.data;
   const { target, status } = attack(gameId, indexPlayer, { x, y });
-  if (status === 'killed') {
+  if (status === 'killed' && target) {
     const splashCoord = calcSplashWave(target);
-    //   type: 'attack_multiple',
-    //   resArr:
-    //
-  } else
-    return {
-      type: 'attack',
-      data: {
-        position: {
-          x,
-          y,
-        },
-        currentPlayer: indexPlayer,
-        status: status,
+  } else return {
+    type: 'attack',
+    data: {
+      position: {
+        x,
+        y,
       },
-      id: req.id,
-    };
+      currentPlayer: indexPlayer,
+      status: status,
+    },
+    id: req.id,
+  };
 }
 
 export default gameHandler;
